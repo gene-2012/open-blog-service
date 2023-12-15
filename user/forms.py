94521d12d8bb1bba38fb9b6c7684d8e07733b271
gene-2012 <gene201248@126.com>
2023-12-15 20:17:46 +0800

    delete avatar image

--- user/forms.py
+++ user/forms.py
@@ -5,6 +5,6 @@ from django.contrib.auth import get_user_model
 class PageForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ['username', 'page', 'avatar']
-        labels = {'username': 'username:', 'page': 'page:', 'avatar': 'avatar:'}
-        widgets = {'username': forms.TextInput(),'page': forms.Textarea(),'avatar':forms.ImageField()}
+        fields = ['username', 'page', ]
+        labels = {'username': 'username:', 'page': 'page:', }
+        widgets = {'username': forms.TextInput(),'page': forms.Textarea()}
